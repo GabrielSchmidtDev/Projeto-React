cadb23e101c4a21de4fcd61fd8c470ce03b9fd4f
GabrielSchmidtDev <165727851+GabrielSchmidtDev@users.noreply.github.com>
2024-11-30 21:42:21 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",  // Adicione isso para que o Tailwind saiba onde procurar as classes.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
